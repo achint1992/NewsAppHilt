72c2e3ba09fd2100215d0d46a90e61cce71e07a6
achintdigiscribe <45483706+achintdigiscribe@users.noreply.github.com>
2022-04-19 17:58:23 +0530

    cdci Commit 1

--- app/src/main/java/com/example/newsapphilt/MainActivity.kt
+++ app/src/main/java/com/example/newsapphilt/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         fragmentController?.let { binding.bottomNav.setupWithNavController(it) }
         viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
         Log.e("trying new Branch ","commit")
+        Log.e("Changing","Commit 1")
     }
 
 
