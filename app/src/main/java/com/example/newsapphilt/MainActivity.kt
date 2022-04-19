0ab14d8445434774b24728b0fea4fd76c5aaa405
achintdigiscribe <45483706+achintdigiscribe@users.noreply.github.com>
2022-04-19 17:18:00 +0530

    Revert "This is error commit"
    This reverts commit ce727b02

--- app/src/main/java/com/example/newsapphilt/MainActivity.kt
+++ app/src/main/java/com/example/newsapphilt/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         fragmentController?.let { binding.bottomNav.setupWithNavController(it) }
         viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
         Log.e("trying second ","commit")
-        asdfadfasdgasga
     }
 
 
